f467fa832764aa79f171d5c9d79483200e7d5cc3
RomelSingh <Romel.Singh@EmotionStudios.co.za>
2024-07-02 05:53:35 +0200

    updated initial setup script

--- Scripts/InteractiveMVC_Tables_With_Data.sql
+++ Scripts/InteractiveMVC_Tables_With_Data.sql
@@ -1,3 +1,124 @@
+USE [master]
+GO
+
+/****** Object:  Database [PersonAccountTransactions]    Script Date: 2024/07/02 05:32:39 ******/
+CREATE DATABASE [PersonAccountTransactions]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'person_account_trans_db', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\test_db.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'person_account_trans_db_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\test_db_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [PersonAccountTransactions].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET AUTO_CLOSE ON 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET  ENABLE_BROKER 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET QUERY_STORE = ON
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+
+ALTER DATABASE [PersonAccountTransactions] SET  READ_WRITE 
+GO
+
+USE [PersonAccountTransactions]
+GO
+
 CREATE TABLE Persons(
   code int not null identity(1,1),
   name varchar(50) null,
